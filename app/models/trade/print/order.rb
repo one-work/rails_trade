5178365163b1c1939eb6ea9644c2297f5fa19dae
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-12 22:15:37 +0800

    print

--- app/models/trade/print/order.rb
+++ app/models/trade/print/order.rb
@@ -61,7 +61,9 @@ module Trade
           pr.text_big("#{item.good_name} x #{item.number.to_human}") if item.good
           pr.text_big item.desk.name if item.desk
           pr.text "#{item.class.human_attribute_name(:created_at)}：#{item.created_at.to_fs(:wechat)}"
+          [item.good_name, item.number.to_human]
         end
+        pr.table_big(headers: { '品名' => 8, '数量' => nil }, cols: cols)
       when 'delivery_prepare'
         items.where(dispatch: [nil, 'delivery']).each do |item|
           pr.text_big("#{item.good_name} x #{item.number.to_human}") if item.good
