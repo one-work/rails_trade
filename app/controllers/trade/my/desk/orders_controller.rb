5822e05eb583317d7093d7daa06811e20b89f175
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-18 16:44:40 +0800

    fixed

--- app/controllers/trade/my/desk/orders_controller.rb
+++ app/controllers/trade/my/desk/orders_controller.rb
@@ -8,19 +8,8 @@ module Trade
       q_params.merge! default_params
       q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :desk_id, :aim, :address_id, :status)
 
-      @order_s = Order.where(user_id: [nil, current_user.id]).default_where(q_params)
+      @order_s = Order.default_where(q_params)
       @orders = @order_s.includes(items: { good: { logo_attachment: :blob } }).order(id: :desc).page(params[:page]).per(params[:per])
-    end
-
-    def confirm
-      q_params = {
-        state: ['init']
-      }
-      q_params.merge! default_params
-      q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :desk_id, :aim, :address_id, :status)
-
-      @orders = Order.where(user_id: nil).default_where(q_params)
-      @orders.update(user_id: current_user.id)
     end
 
   end
