1fbcf36aa12adb6466d0c2c42ac5dda1f85a2cb0
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-18 15:42:05 +0800

    show all orders

--- app/controllers/trade/my/desk/orders_controller.rb
+++ app/controllers/trade/my/desk/orders_controller.rb
@@ -8,7 +8,7 @@ module Trade
       q_params.merge! default_params
       q_params.merge! params.permit(:cart_id, :order_id, :good_type, :good_id, :desk_id, :aim, :address_id, :status)
 
-      @order_s = Order.where(user_id: nil).default_where(q_params)
+      @order_s = Order.where(user_id: [nil, current_user.id]).default_where(q_params)
       @orders = @order_s.includes(items: { good: { logo_attachment: :blob } }).order(id: :desc).page(params[:page]).per(params[:per])
     end
 
